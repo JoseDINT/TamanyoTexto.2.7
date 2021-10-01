26a0c6ef736152297ef820c35d11f8e8fad029b4
jose <jl.rodriguez@iesdoctorbalmis.com>
2021-10-01 11:55:44 +0200

    Agregar archivos de proyecto.

---
+++ TamanyoTexto.2.7/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TamanyoTexto._2._7
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FormatoButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton opcion = (RadioButton)sender;
+            TextoFormateadoTextBlock.FontSize = double.Parse(opcion.Tag.ToString());
+
+        }
+    }
+}
